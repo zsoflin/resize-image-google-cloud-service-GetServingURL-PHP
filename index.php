7e3187a440a31e05b21711f220394678b1c5dc2a
Zach Soflin <soflins@gmail.com>
2016-07-07 18:51:19 +0000

    made size a integer

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use google\appengine\api\cloud_storage\CloudStorageTools;
 syslog(LOG_WARNING, "Imported Cloud Storage Tools");
 //var_dump( $_GET);
 $imageName=$_GET["image"];
-$size=$_GET["size"];
+$size=(int)$_GET["size"];
 syslog(LOG_WARNING, "Object URL $object_url");
 syslog(LOG_WARNING, "Size $size");
 echo("test");
