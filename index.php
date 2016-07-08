f0bbd4e0af45f572e50691d8716d30649d30d3c9
Zach Soflin <soflins@gmail.com>
2016-07-08 12:39:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,11 @@ syslog(LOG_WARNING, "Imported Cloud Storage Tools");
 $imageName=$_GET["image"];
 $size=(int)$_GET["size"];
 $project=$_GET["project"];
+$cropped=($_GET["cropped"] === 'true');
 syslog(LOG_WARNING, "Object URL $object_url");
 syslog(LOG_WARNING, "Size $size");
 $imageUrl="gs://project-5945470096966003818.appspot.com/projects/$project/full/$imageName";
-$resized_image_url = CloudStorageTools::getImageServingUrl($imageUrl,['size' => $size, 'crop' => false]);
+$resized_image_url = CloudStorageTools::getImageServingUrl($imageUrl,['size' => $size, 'crop' => $cropped]);
 syslog(LOG_WARNING, "Output Url $resized_image_url");
 header("location: $resized_image_url");
 
